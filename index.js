ccb592625885a4868846866298f5a896412dda1b
Gaurang Kanbur <gaurangkanbur@gmail.com>
2026-06-19 12:34:12 +0530

    v1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Server is Healthy'});
+    return res.json({message: 'Serverv1 is Healthy'});
 })
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
