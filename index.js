c8365bc99f2950b577634ed63531c604aa1715e8
Gaurang Kanbur <gaurangkanbur@gmail.com>
2026-06-19 12:54:21 +0530

    Github Actions

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Serverv1 is Healthy'});
+    return res.json({message: 'Server is Healthy v4.1', status: 'Github Actions'});
 })
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
