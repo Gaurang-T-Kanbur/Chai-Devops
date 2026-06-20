edef7608f600d2254cacb00c07246d8f71579257
Gaurang Kanbur <gaurangkanbur@gmail.com>
2026-06-20 10:56:44 +0530

    Docker Chal Gyagit add .!

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Server is Healthy v4.1', status: 'Github Actions Hurray!!!', });
+    return res.json({message: 'Server is Healthy v4.1', status: 'Docker Chal Gya 🐳', });
 })
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
