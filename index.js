63603e5a06df4597c709a972d3a66e6d50e02db5
Gaurang Kanbur <gaurangkanbur@gmail.com>
2026-06-20 11:50:19 +0530

    Caddy Chal Gya

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.get('/', (req, res) => {
-    return res.json({message: 'Server is Healthy v5.13', status: 'Docker ke saath Caddy Chal Gya 🐳🔥', });
+    return res.json({message: 'Server is Healthy v5.139', status: 'Docker ke saath Caddy Chal Gya 🐳🔥', });
 })
 
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`))
